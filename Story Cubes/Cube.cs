4778e42fdd78a033931bce06097040986d1ca5b9
Skwangles <skwanglesavenue@gmail.com>
2020-05-30 18:32:01 +1200

    Add project files.

---
+++ Story Cubes/Cube.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story_Cubes
+{
+    class Cube
+    {
+        public Cube(string img1, string img2, string img3, string img4, string img5, string img6)
+        {
+            
+            this.ImagePaths[0] = img1;
+            this.ImagePaths[1] = img2;
+            this.ImagePaths[2] = img3;
+            this.ImagePaths[3] = img4;
+            this.ImagePaths[4] = img5;
+            this.ImagePaths[5] = img6;
+           
+
+        }
+        
+        
+        private string[] ImagePaths = new string[6];
+
+        public string RandomPic()
+        {
+            return ImagePaths[RandomNumber(0, 6)];
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Story Cubes/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Story_Cubes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Cube[] Cubes = new Cube[9];
+
+        
+    string[,] Cube2 = new string[,]{
+  { Properties.Resource.Cs,"" },
+  { "","" },
+  { "","" },
+  { "",""  },
+  { "","" }
+};
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Box1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private Cube CreateCubes(int number, string img1, string img2, string img3, string img4, string img5, string img6, string img7, string img8, string img9)
+        {
+            Cubes[number] = new Cube(img1, img2, img3, img4, img5, img6);
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) ½1r2t3e5
+        {
+            
+        }
+}
+}
